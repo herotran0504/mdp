180bb2f3800010fc5d6e553e71be25f00df37895
herotran0504 <herotran0504@gmail.com>
2024-03-03 21:20:11 -0600

    Add assignment 3

---
+++ app/src/main/java/miu/mdp/assignment3/WaterResource.kt
@@ -0,0 +1,3 @@
+package miu.mdp.assignment3
+
+data class WaterResource(val resName: String, val resLocation: String, val resType: String)
--- core/src/main/java/miu/mdp/core/ActivityExt.kt
+++ core/src/main/java/miu/mdp/core/ActivityExt.kt
@@ -4,10 +4,15 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 fun Activity.hideKeyboard() {
     findViewById<View>(android.R.id.content)?.let {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
         imm?.hideSoftInputFromWindow(it.windowToken, 0)
     }
+}
+
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
